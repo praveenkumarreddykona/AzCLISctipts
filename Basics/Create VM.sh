f550d8c09c322ad7c24dbfdbf86cfb5218403944
Praveen Kumar Reddy Kona <praveen.kumar.reddy.kona@quest.com>
2021-11-16 07:40:25 +0530

    changed file

--- Basics/Create VM.sh
+++ Basics/Create VM.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
+read sizequery
+az vm list-sizes -l eastus2 -o table | grep $sizequery
+echo 'Enter the size selected'
+read size
+az vm image list -o table
+echo 'Enter the size selected'
+read image
+echo -n 'Enter username'
+read username
+echo -n 'Enter Password'
+read -s password
 
-az vm list-sizes -l eastus2 -o table | grep B
-az vm create -n testvm01 -l eastus2 -g AzLearnRG-01 --size Standard_B2ms --admin-username sysadm --admin-password "Password" --image MicrosoftWindowsServer:WindowsServer:2019-Datacenter:latest
+az vm create -n testvm01 -l eastus2 -g AzLearnRG-01 --size $size --admin-username $username --admin-password $password --image $image
