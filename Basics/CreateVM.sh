ca5126dce6cf999bde71b8fe645080b1e4664224
Praveen Kumar Reddy Kona <praveen.kumar.reddy.kona@quest.com>
2021-11-16 13:08:35 +0530

    updated createVM.sh

--- Basics/CreateVM.sh
+++ Basics/CreateVM.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+az account list-locations -o table
+echo 'Enter Region'
+read region
+echo 'Enter VM name'
+read name
+echo 'Enter what size you you want to deploy the vM'
 read sizequery
 az vm list-sizes -l eastus2 -o table | grep $sizequery
 echo 'Enter the size selected'
@@ -9,6 +15,6 @@ read image
 echo -n 'Enter username'
 read username
 echo -n 'Enter Password'
-read -s password
+read password
 
-az vm create -n testvm01 -l eastus2 -g AzLearnRG-01 --size $size --admin-username $username --admin-password $password --image $image
+az vm create -n $name -l $region -g AzLearnRG-01 --size $size --admin-username $username --admin-password $password --image $image
